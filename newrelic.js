612fa1e4606efef98a01b823f65f9a0429fe507e
Tobias Abarbanell <tobias@abarbanell.de>
2015-01-08 20:34:16 +0100

    added newrelic

---
+++ newrelic.js
@@ -0,0 +1,24 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name : ['loopnav'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key : 'b74fe15d8ad31aa0bf6fb9291708651d069e014f',
+  logging : {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level : 'info'
+  }
+};
