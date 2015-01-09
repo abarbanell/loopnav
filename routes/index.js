6153b015616bbe93ba7601e55c561b47b79605c1
Tobias Abarbanell <tobias.abarbanell@frontiersin.org>
2015-01-09 11:39:32 +0100

    added coauthors

--- routes/index.js
+++ routes/index.js
@@ -8,13 +8,35 @@ router.get('/', function(req, res) {
 });
 
 router.get('/user/:id', function(req, res) {
-	loop_api.main_info(req.params.id, function(err, result) {
-		console.log('api call returned.');
+	loop_api.main_info(req.params.id, function(err, minfo) {
+		console.log('main-info api call returned.');
 		if (err) {
-			res.render('pages/error', { title: 'loopnav error', message: 'API call error' , error: {status: JSON.stringify(err), stack: 'no stack information available'}});
+			res.render('pages/error', { 
+				title: 'loopnav error', 
+				message: 'API call error' , 
+				error: {
+					status: JSON.stringify(err), 
+					stack: 'no stack information available'
+				}
+			});
 		} else {
-			console.log('result: ' + result);
-			res.render('pages/user', { title: 'loopnav', user: result });
+			console.log('result: ' + minfo);
+			loop_api.co_authors(req.params.id, function(err, coauth) {
+				console.log('co-authors api call returned.');
+				if (err) {
+					res.render('pages/error', { 
+						title: 'loopnav error', 
+						message: 'API call error' , 
+						error: {
+							status: JSON.stringify(err), 
+							stack: 'no stack information available'
+						}
+					});
+				} else {
+					console.log('result: ' + coauth);
+					res.render('pages/user', { title: 'loopnav', user: minfo, co_authors: coauth });
+				}
+			});
 		}
 	});
 });
--- util/loop_api.js
+++ util/loop_api.js
@@ -8,8 +8,16 @@ var loop_api = function() {
 		console.log('url: ' + url);
 		api.get(url, callback);
 	}
+
+	var co_authors = function(id, callback) {
+		var url = "http://loop.frontiersin.org/api/v1/users/" + id + "/co-authors";
+		console.log('url: ' + url);
+		api.get(url, callback);
+	}
+
 	return {
-		main_info: main_info
+		main_info: main_info,
+		co_authors: co_authors
 	}
 }();
 
