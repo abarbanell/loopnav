32f6038cd5208a594f6cb2420bcead9d00aa2eeb
Tobias Abarbanell <tobias@abarbanell.de>
2015-01-23 21:27:11 +0000

    some data inserted in user collection, but not retrieved correctly

--- .c9/metadata/preview-https:/loopnav-abarbanell-1.c9.io
+++ .c9/metadata/preview-https:/loopnav-abarbanell-1.c9.io
@@ -1,1 +1,1 @@
-{"changed":false,"filter":false,"title":"[P] https://loopnav-abarbanell-1.c9.io","value":"","undoManager":{"mark":0,"position":-1,"stack":[]},"preview":{"path":"https://loopnav-abarbanell-1.c9.io","previewer":"preview.browser"},"timestamp":1422007399828}
+{"filter":false,"title":"[P] https://loopnav-abarbanell-1.c9.io","undoManager":{"mark":0,"position":-1,"stack":[]},"preview":{"path":"https://loopnav-abarbanell-1.c9.io/","previewer":"preview.browser"},"timestamp":1422007399828,"hash":"da39a3ee5e6b4b0d3255bfef95601890afd80709"}
--- model/users.js
+++ model/users.js
@@ -2,23 +2,63 @@
 var db = require('../util/db');
 db.bind('user');
 db.user.ensureIndex([['userId', 1]], true, function(err, replies){});
+var loop_api = require('../util/loop_api');
 
 var user = function() {
 
 	var get = function(id, callback) {
 		db.user.findOne({userId: id}, callback);
-	}	
+	};	
 
 	var load = function(base, count, callback) {
-    console.log('model.users.load(): base=' + base + ', count=' + count);
-		for (var id = base; id < base + count; id++) {
-			db.user.findOne({userId: id}, function(err, res) {
-		        if (!res) {
-		          console.log('model.users.load(): need to fetch ' + id + ' from loop_api');
-		        }
-		    });
-		}
-	}	
+	  loadOne(base, count, callback);
+	};
+	  
+	var loadOne = function(base, count, callback) {
+ 	  console.log('model.users.load(): base=' + base + ', count=' + count);
+	  if (count == 0) {
+		console.log('model.users.loadOne - done');
+		return (callback()); //TODO do we need to pass err and result?
+	  } else {
+	  	db.user.findOne({userId: base}, function(err, res) {
+		  if (err) {
+		  	return callback(err, null);
+		  }
+		  if (res) {
+		    console.log('model.users.load(): found ' + base);
+		    console.log('model.users.load(): res = ' + JSON.stringify(res));
+		    //TODO: check for picture and decide next step
+		    return loadOne(base+1, count-1, callback);
+		  } else {
+		    console.log('model.users.load(): not found ' + base);
+		    // here we try to get the data from api call
+		    loop_api.main_info(base, function(err, result) {
+		   	  console.log('model.users.loadOne: api result = ' + JSON.stringify(result)) 	
+    		  var obj = {user: base};
+    		  if (err || !result) {
+    		    // not found
+    			obj.status = 'not-found';
+    		  } else {
+        	    if (result.hasOwnProperty('ProfilePicture') && result.ProfilePicture.toString().contains('Thumb_203_203')) {
+          		  obj.status = 'ok-with-pic';
+        	      obj.ProfilePicture = result.ProfilePicture;
+      			} else {
+        		  obj.status = 'no-pic';
+    			}
+    	      }  
+    	      console.log('model.users.loadOne: status=' + obj.status + ' for id=' + obj.user);
+    	      db.user.save(obj, function(err, res) {
+    		    if (err) {
+    			  return callback(err, null);
+    			} else {
+    			  return loadOne(base+1, count-1, callback);
+    			}	
+    	      });
+  			});
+		  }
+		});
+	  }
+	}; 	
 
 	return {
 		get: get,
