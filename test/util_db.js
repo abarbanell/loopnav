a3d968ffc0b52bf2e0c669691c2c279de62adfea
Tobias Abarbanell <tobias.abarbanell@frontiersin.org>
2015-03-11 10:31:07 +0100

    tests prove we have no DB connection

--- test/util_db.js
+++ test/util_db.js
@@ -3,27 +3,59 @@
 
 var expect = require('expect.js');
 
-var sut = require('../util/db');
-
 describe('util/db tests', function() {
-  it('check some syntax of expect.js', function() {
+  it('check some syntax of expect.js', function(done) {
     var f = function(){};
     expect(f).to.be.an('function');
     expect(f.doesnotexist).to.not.be.an('function'); // should fail
     expect(f.bind).to.be.an('function'); // Function.prototype.bind()
+		done();
   })
 
-  it('check default db object', function(){
-	  expect(sut).to.be.an('function');
-    expect(sut.bind).to.be.an('function'); // Function.prototype.bind()
-  });
-  
-  it('can we open a DB?', function(){
-		var db = sut('mongodb://localhost/test');
-		console.log(db);
-    expect(db).to.be.ok()
-		expect(db.connection).to.be.an('function');
-  })
-  
+  it('can we open a DB from mongoskin directly?', function(done){
+		var mongo = require('mongoskin');
+		var db = mongo.db('mongodb://localhost:27017/test');
+		// console.log(db);
+    expect(db).to.an('object');
+		expect(db.bind).to.be.an('function');
+		db.bind('testcollection');
+		expect(db.testcollection).to.be.an('object');
+		expect(db.testcollection.find).to.be.an('function');
+		db.testcollection.find().toArray(function(err, items) {
+			expect(err).to.not.be.ok();
+			expect(items).to.be.an('array');
+			done();
+		});
+	});
+
+	it('can we open a DB via MONGOLAG_URI in util/db.js?', function(done) {
+		process.env.MONGOLAB_URI = 'mongodb://localhost:27017/test';
+		var db = require('../util/db');
+    expect(db).to.be.an('object');
+		expect(db.bind).to.be.an('function');
+		db.bind('testcollection');
+		expect(db.testcollection).to.be.an('object');
+		expect(db.testcollection.find).to.be.an('function');
+		db.testcollection.find().toArray(function(err, items) {
+			expect(err).to.not.be.ok();
+			expect(items).to.be.an('array');
+			done();
+		});
+	});  
+
+	it('can we open a DB via default connection in util/db.js?', function() {
+		delete process.env.MONGOLAB_URI;
+		var db = require('../util/db');
+    expect(db).to.be.an('object');
+		expect(db.bind).to.be.an('function');
+		db.bind('testcollection');
+		expect(db.testcollection).to.be.an('object');
+		expect(db.testcollection.find).to.be.an('function');
+		db.testcollection.find().toArray(function(err, items) {
+			expect(err).to.not.be.ok();
+			expect(items).to.be.an('array');
+			done();
+		});
+	});  
 });
  
