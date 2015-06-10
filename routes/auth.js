ce038eec468a4afd33a3f1c6631eef33916da021
Tobias Abarbanell <tobias.abarbanell@frontiersin.org>
2015-06-10 17:28:24 +0200

    added login routes to dummy page

--- routes/auth.js
+++ routes/auth.js
@@ -1,17 +1,23 @@
 var logger = require('../util/logger');
 var express = require('express');
 var router = express.Router();
-// var users = require('../model/users.js');
+var passport = require('passport');
+var oauth2Strategy = require('passport-oauth2').Strategy;
 
-if (!('contains' in String.prototype)) {
-  String.prototype.contains = function(str, startIndex) {
-    return ''.indexOf.call(this, str, startIndex) !== -1;
-  };
-}
 
 /* Loop auth callback. */
 router.get('/callback', function(req, res) {
   res.render('pages/authcallback', { title: 'Loopnav Auth Callback' } );
+});
+
+/* show the login page */
+router.get('/login', function(req, res) {
+	res.render('pages/index', { title: 'GET - this should be the login page' } );
+});
+
+/* POST from the login page */
+router.post('/login', function(req, res) {
+	res.render('pages/index', { title: 'POST - this should be the login page' } );
 });
 
 module.exports = router;
